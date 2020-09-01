fb1513dc0fe91ee36757f25a0f717cdb7baff0fa
Robert Young <Robert.T.Young-1@ou.edu>
2020-09-01 18:06:21 -0500

    completed the student uml and did DateTime stuff

---
+++ Review Problems/ClassesAndFiles/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndFiles
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public DateTime enrollDate { get; set; }
+        public static int totalStudents { get; set; }
+
+        public Student(string name, int id) : this()
+        {
+            this.name = name;
+            this.id = id;
+            //this.enrollDate = DateTime.Now;
+            totalStudents++;
+        }
+
+        public Student()
+        {
+            name = string.Empty;
+            id = -1;
+            enrollDate = DateTime.Now;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return ($"{name} has an id of {id}."); 
+        }
+
+    }
+}

a4b9d46438b226f3609b29dd3641c66258fcb668
Robert Young <Robert.T.Young-1@ou.edu>
2020-09-03 17:44:37 -0500

    created files example and completed it

---
+++ Review Problems/ReadingFilesExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace ReadingFilesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Carol\Downloads\Pride and Prejudice by Jane Austen (1).txt";
+
+            //string entireBook = File.ReadAllText(filePath);
+            string[] allLines = File.ReadAllLines(filePath);
+
+            //Output 5 lines of the book at a time
+            //for (int i = 0; i < allLines.Length; i++)
+            //{
+            //    if (i % 5 == 0 && i != 0)
+            //    {
+            //        Console.ReadKey();
+            //    }
+            //    Console.WriteLine(allLines);
+            //}
+
+            //Output a Chapter at a time
+            foreach (var line in allLines)
+            {
+                if (line.ToLower().Contains("chapter") == true)
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(line);
+            }
+
+
+
+
+        }
+    }
+}

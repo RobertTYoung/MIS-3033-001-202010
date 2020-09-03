df8279b87fdb4faeaa95e3921c9dbb5932834fe4
Robert Young <Robert.T.Young-1@ou.edu>
2020-09-03 18:12:47 -0500

    Finished most of harder student example

--- Review Problems/MoreDifficultStudentExample/Program.cs
+++ Review Problems/MoreDifficultStudentExample/Program.cs
@@ -6,7 +6,31 @@ namespace MoreDifficultStudentExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("enter a number");
+            //double x = Convert.ToDouble(Console.WriteLine());
+
+            Student myStudent = new Student(5, "Robert", "Young", 150000);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            
+
+            try
+            {
+                myStudent.MakePayment(500);
+                Console.WriteLine(myStudent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
         }
     }
 }

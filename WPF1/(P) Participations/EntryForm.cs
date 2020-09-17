e74a5c8e6775396fb192e07f40d5850287b80cae
Robert Young <Robert.T.Young-1@ou.edu>
2020-09-17 17:24:14 -0500

    created participation project with WPF1 in it and began making WPF and class

---
+++ WPF1/(P) Participations/EntryForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__Participations
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm();
+        public EntryForm(string name, string address, int zipCode)
+        override public ToString()
+        {
+            return string;
+        }
+
+        
+    }
+}

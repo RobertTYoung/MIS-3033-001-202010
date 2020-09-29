50a534c83f5a4d72a6f7aa7fe6d3efad778dd16c
Robert Young <Robert.T.Young-1@ou.edu>
2020-09-29 18:06:02 -0500

    started wroking on next 2 classes

---
+++ Review Problems/(P) JSON - Pokemon/PokemonStatsAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__JSON___Pokemon
+{
+    class PokemonStatsAPI
+    {
+        public Info info { get; set; }
+    }
+    public class Info
+    {
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public string FrontDefault { get; set; }
+        public string BackDefault { get; set; }
+
+    }
+}

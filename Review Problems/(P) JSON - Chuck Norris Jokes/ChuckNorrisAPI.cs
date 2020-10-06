b8d256c57e4d6ba60bb391cbd380eece77ee5f9f
Robert Young <Robert.T.Young-1@ou.edu>
2020-10-06 17:33:25 -0500

    Created combo box in chuck norris participation

---
+++ Review Problems/(P) JSON - Chuck Norris Jokes/ChuckNorrisAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__JSON___Chuck_Norris_Jokes
+{
+    class ChuckNorrisAPI
+    {
+        public List<Category> results { get; set; }
+    }
+    public class Category
+    { 
+        public string value { get; set; }
+    }
+}
---
+++ Review Problems/(P) JSON - Chuck Norris Jokes/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P__JSON___Chuck_Norris_Jokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = @"https://api.chucknorris.io/jokes/categories";
+            ChuckNorrisAPI api;
+            using (var client = new HttpClient())
+            {
+                //string results = client.GetStringAsync(url).Result;
+
+                //api = JsonConvert.DeserializeObject<ChuckNorrisAPI>(results);
+
+                string jsonResults = client.GetStringAsync(url).Result;
+
+                string[] cats = JsonConvert.DeserializeObject<String[]>(jsonResults);
+
+                foreach (var category in cats)
+                {
+                    cboCategory.Items.Add(category);
+                }
+
+            }
+
+            //foreach (var category cats)
+            //{
+            //    cboCategory.Items.Add(category);
+            //}
+
+            //jsonResults = client.GetStringAsync(@"https://api.chucknorris.io/jokes/categories").Results;
+
+            //string[] cats = JsonConvert.DeserializeObject<String[]>(jsonResults);
+        }
+
+        private void btnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

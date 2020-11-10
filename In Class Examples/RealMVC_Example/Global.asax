e80b5420ac8ab3bc56e47de746f69109597173c0
Robert Young <Robert.T.Young-1@ou.edu>
2020-11-10 17:26:09 -0600

    started MVC example

---
+++ In Class Examples/RealMVC_Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealMVC_Example.MvcApplication" Language="C#" %>
--- Review Problems/(P) Classes 3/Student.cs
+++ Review Problems/(P) Classes 3/Student.cs
@@ -8,5 +8,20 @@ namespace _P__Classes_3
 {
     class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+        public Address Address { get; set; }
+
+        Student()
+        { 
+        
+        }
+
+        Student(string firstName, string lastName, string major, double gpa)
+        { 
+        
+        }
     }
 }

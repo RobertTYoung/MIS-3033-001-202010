6e36bb706e9976dd12c09a6a789d6a2147b6afa7
Robert Young <Robert.T.Young-1@ou.edu>
2020-11-12 17:40:43 -0600

    finsihed chuck norris and starting pokemon

---
+++ Review Problems/(P) MVC - Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,36 @@
+﻿using _P__MVC___Beginner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _P__MVC___Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonAPI allpokemon;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("http://pokeapi.co/api/v2/pokemon?offset0&limit=1100").Result;
+
+                allpokemon = JsonConvert.DeserializeObject<PokemonAPI>(json);
+            }
+
+
+            return View(allpokemon.results);
+        }
+
+        public ActionResult Info(string id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Review Problems/(P) MVC - Beginner/Models/PokemonAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _P__MVC___Beginner.Models
+{
+    public class PokemonAPI
+    {
+        public List<Pokemon> results { get; set; }
+    }
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name[0].ToString().ToUpper() + name.Substring(1, name.Length - 1);
+        }
+    }
+}

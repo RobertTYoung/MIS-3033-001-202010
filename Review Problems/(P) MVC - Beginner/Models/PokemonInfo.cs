ba3a560d918196a6f87f607e2d59bc01a2ab3378
Robert Young <Robert.T.Young-1@ou.edu>
2020-11-17 15:04:08 -0600

    started filling out pokemon info class

--- Review Problems/(P) MVC - Beginner/Models/PokemonInfo.cs
+++ Review Problems/(P) MVC - Beginner/Models/PokemonInfo.cs
@@ -7,7 +7,25 @@ namespace _P__MVC___Beginner.Models
 {
     public class PokemonInfo
     { 
-    
+        public List<Ability> abilities { get; set; }
+        public int base_experience { get; set; }
+        public List<Form> forms { get; set; }
+        public List<GameIndice> game_indicies { get; set; }
+        public int height { get; set; }
+        public List<HeldItem> held_items { get; set; }
+        public int id { get; set; }
+        public bool is_default { get; set; }
+        public string location_area_encounter { get; set; }
+        public List<Move> moves { get; set; }
+        public string name { get; set; }
+        public int order { get; set; }
+        public Species species { get; set; }
+        public Sprites sprites { get; set; }
+        public List<Stat> stats { get; set; }
+        public List<Type> types { get; set; }
+        public int weight { get; set; }
+
+
     }
 
 }
